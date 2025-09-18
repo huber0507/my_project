60781bd048719783086b2098c82e1a37a7394d07
huber0507 <jiangw2858@163.com>
2025-09-18 16:54:55 +0800

    upload

--- 07_key/project/main.c
+++ 07_key/project/main.c
@@ -8,12 +8,30 @@
  * @description	: main函数
  * @param 		: 无
  * @return 		: 无
+ * 
+ * 测试日志：20205/09/17晚间测试发现我初始状态LED灯就是打开的
+ * 我明明在main函数里关闭了，但是实际上还是打开了
+ * 我发现可能由于led_init函数里默认打开了
+ * 因为关闭LED是初始化之前
+ * 一旦LED初始化之后可能就会直接打开LED灯了
+ * 9/18继续测试
+ * *
+ * *
+ * *
+ * *
+ * *
+ * 
+ *
+ * 
+ * 
+ *  *
+ * 
  */
 int main(void)
 {
 	int i = 0;
 	int keyvalue = 0;
-	unsigned char led_state = OFF;  // LED初始状态：灭
+	// unsigned char led_state = OFF;  // LED初始状态：灭
 	unsigned char beep_state = OFF;
 	
 	clk_enable();		/* 使能所有的时钟 			*/
